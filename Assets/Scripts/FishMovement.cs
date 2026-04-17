2c9b96b5295055e5dbcfdba1e3b2dd8f259d179a
Jessica Tong <jessicatong14@gmail.com>
2026-04-17 22:24:25 +0200

    working fish movement for different reeling states

--- Assets/Scripts/FishMovement.cs
+++ Assets/Scripts/FishMovement.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class FishMovement : MonoBehaviour
 {
-
-    public static event Action LeavingLineRange;
-    public static event Action EnteringLineRange;
-
-    private Animator animator;
-
-    private Fish fish;
-    public FishingManager fishManager;
-    private Vector3 position;
-    private Vector3 targetPosition;
-    private float swimmingSpeed;
-
     // will likely have to adjust these based on the camera view 
     public float xLeftBoundary = -15f;
     public float xRightBoundary = 15f;
-    public float xLineLeftRange = -5f;
-    public float xLineRightRange = 5f;
-
+    public float lineRange = 5f;
     public float arrivalThreshold = 0.1f;
+    public float idleSwimmingSpeed = 2f;
 
-
+    private Fish fish;
+    private float reelingSwimmingSpeed;
     private int direction = 1; // 1 for right, -1 for left
+    private Vector3 position;
+    private Vector3 targetPosition;
 
+    private FishingManager fishManager;
+    private enum FishingState
+    {
+        Idle,
+        Casting,
+        Reeling,
+        Caught,
+        Escaped
+    }
+    private FishingState currentState;
 
 
 
@@ -34,30 +34,91 @@ public class FishMovement : MonoBehaviour
     void Start()
     {
         fish = GetComponent<Fish>();
-        animator = GetComponent<Animator>();
-
-        // FishingManager.OnCast += HandleCast;
-        FishingManager.OnBite += HandleBite;
-        // FishingManager.OnReelingActive += HandleReelingActive;
-        // FishingManager.OnReelingInactive += HandleReelingInactive;
-        // FishingManager.OnCaught += HandleCaught;
-        // FishingManager.OnLineBreak += HandleLineBreak;
-        // FishingManager.OnWiggle += HandleWiggleStart;
-        // FishingManager.OffWiggle += HandleWiggleEnd;
+        currentState = FishingState.Idle;
         position = transform.position;
-        swimmingSpeed = GetComponent<Fish>().swimmingSpeed;
+        reelingSwimmingSpeed = GetComponent<Fish>().swimmingSpeed;
+
+        FishingManager.OnBite += OnBite;
+        FishingManager.OnHooked += OnHooked;
+        FishingManager.OnCaught += OnCaught;
+        FishingManager.OnEscaped += OnEscaped;
+
         SetTargetPosition(position);
         Debug.Log("Initial Position: " + position);
 
     }
 
-    // Update is called once per frame
+    private void OnBite() => currentState = FishingState.Casting;
+    private void OnHooked() => currentState = FishingState.Reeling;
+    private void OnCaught() => currentState = FishingState.Caught;
+    private void OnEscaped() => currentState = FishingState.Escaped;
+
+    void OnDestroy()
+    {
+        FishingManager.OnBite -= OnBite;
+        FishingManager.OnHooked -= OnHooked;
+        FishingManager.OnCaught -= OnCaught;
+        FishingManager.OnEscaped -= OnEscaped;
+    }
+
     void Update()
     {
-        // Debug.Log("Current Position: " + transform.position);
-        SwimTowardTarget();
-        CheckInLineRange();
 
+        switch (currentState)
+        {
+            case FishingState.Idle: HandleIdleMovement(); break;
+            case FishingState.Casting: HandleCastingMovement(); break;
+            case FishingState.Reeling: HandleReelingMovement(); break;
+            case FishingState.Caught: HandleCaughtMovement(); break;
+            case FishingState.Escaped: HandleEscapedMovement(); break;
+        }
+
+    }
+
+    private void HandleIdleMovement()
+    {
+        Debug.Log("Idle Movement");
+        SwimTowardTarget(idleSwimmingSpeed);
+    }
+
+    private void HandleCastingMovement()
+    {
+        Debug.Log("Casting Movement");
+        if (fish.isActive)
+        {
+            transform.position = new Vector3(0, transform.position.y, transform.position.z); // Move the fish to the center of the screen
+            transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180, transform.eulerAngles.z); // Face the fish forward
+        }
+    }
+    private void HandleReelingMovement()
+    {
+        Debug.Log("Reeling Movement");
+        SwimTowardTarget(reelingSwimmingSpeed);
+        // TODO: functions to check for range
+    }
+
+    private void HandleCaughtMovement()
+    {
+        Debug.Log("Caught Movement");
+        transform.position = new Vector3(0, transform.position.y, transform.position.z); // Move the fish to the center of the screen
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, 180, transform.eulerAngles.z); // Face the fish forward
+        // TODO - make it fly out of the water or add some sort of celebration animation or effect here
+        IEnumerator CaughtAnimation()
+        {
+            // Placeholder for caught animation logic
+            yield return new WaitForSeconds(2f); // Simulate animation duration
+            // TODO: destroy the fish? and unsubscribe from events? or maybe just set it to inactive and move it back to the pool?
+
+        }
+        StartCoroutine(CaughtAnimation());
+
+    }
+
+    private void HandleEscapedMovement()
+    {
+        Debug.Log("Escaped Movement");
+        SetTargetPosition(transform.position); // Set a new target position to swim towards
+        currentState = FishingState.Idle; // Switch back to idle state to swim towards the new target position
     }
 
     public Vector3 SetTargetPosition(Vector3 position)
@@ -65,20 +126,18 @@ public class FishMovement : MonoBehaviour
         if (position.x < 0)
         {
             direction = 1; // Move right
-            targetPosition = new Vector3(UnityEngine.Random.Range(xLineRightRange, xRightBoundary), position.y, position.z);
+            targetPosition = new Vector3(UnityEngine.Random.Range(lineRange, xRightBoundary), position.y, position.z);
         }
         else if (position.x > 0)
         {
             direction = -1; // Move left
-            targetPosition = new Vector3(UnityEngine.Random.Range(xLeftBoundary, xLineLeftRange), position.y, position.z);
+            targetPosition = new Vector3(UnityEngine.Random.Range(xLeftBoundary, -lineRange), position.y, position.z);
         }
-        transform.eulerAngles = new Vector3(transform.eulerAngles.x, direction * 90f, transform.eulerAngles.z); // Flip the fish to face the right direction
-
         Debug.Log("Target Position: " + targetPosition);
         return targetPosition;
     }
 
-    private void SwimTowardTarget()
+    private void SwimTowardTarget(float swimmingSpeed)
     {
         float distanceToTarget = Vector3.Distance(transform.position, targetPosition);
 
@@ -90,27 +149,21 @@ public class FishMovement : MonoBehaviour
             return;
         }
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, swimmingSpeed * Time.deltaTime);
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, direction * 90f, transform.eulerAngles.z); // Flip the fish to face the right direction
 
+        // TODO - add some sort of random vertical movement to make it less linear and more natural
+        // TODO - ease in and out tweening for more natural movement
     }
 
-    private void CheckInLineRange()
+    public bool CheckInLineRange()
     {
-        if (transform.position.x < xLineLeftRange || transform.position.x > xLineRightRange)
+        if (transform.position.x < -lineRange || transform.position.x > lineRange)
         {
-            LeavingLineRange?.Invoke();
+            return false;
         }
         else
         {
-            EnteringLineRange?.Invoke();
-        }
-    }
-
-    public void HandleBite()
-    {
-        if (fish.isActive)
-        {
-            transform.position = new Vector3(0, transform.position.y, transform.position.z); // Move the fish to the center of the screen
-            targetPosition = transform.position; // Set the target position to the center as well
+            return true;
         }
     }
 
