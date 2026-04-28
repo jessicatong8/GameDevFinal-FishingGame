368364593ed7ea4b4470d35e49cb2b09d4ed6ad7
Jessica Tong <jessicatong14@gmail.com>
2026-04-28 17:24:13 +0200

    changed default fish reeling speed

--- Assets/Scripts/Fish Scripts/Fish.cs
+++ Assets/Scripts/Fish Scripts/Fish.cs
@@ -5,7 +5,7 @@ public class Fish : MonoBehaviour
     public string fishName;
     public float swimmingSpeed = 4f;
     public int rarityWeight;
-    public float rarity;
+    public float level;
     public float dripThreshold = 0f;
 
     public bool isActiveFish;
@@ -20,7 +20,7 @@ public class Fish : MonoBehaviour
     [Header("Stats for tension and progress when reeling")]
 
 
-    public float reelingSpeed = 12f;
+    public float reelingSpeed = 5f; //how fast the fish swims when in reeling state
     public float tensionDropRate = 5f;
     public float safeZoneCenter = 50f;
     public float safeZoneWidth = 60f;
