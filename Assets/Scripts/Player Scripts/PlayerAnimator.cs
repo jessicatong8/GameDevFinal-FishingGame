104e82113aba354bc7e5b47dca5f552b44eb6826
Andy Quach <andy.quach@richmond.edu>
2026-04-29 09:24:32 +0200

    null resolution fix

--- Assets/Scripts/Player Scripts/PlayerAnimator.cs
+++ Assets/Scripts/Player Scripts/PlayerAnimator.cs
@@ -20,10 +20,6 @@ public class PlayerAnimator : MonoBehaviour
     public void Awake()
     {
         Instance = this;
-        animator = GetComponentInChildren<Animator>();
-        if (animator == null)        {
-            DebugLogger.Instance.LogWarning("PlayerAnimator: No Animator found in children.");
-        }
+        if (animator == null) animator = GetComponent<Animator>();
     }
-
 }
