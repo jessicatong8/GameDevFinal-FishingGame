9749b77e06979b10843d697ec9334d1049923726
Jessica Tong <jessicatong14@gmail.com>
2026-05-01 16:13:31 +0200

    changed to stack hats on top of each other

--- Assets/Scripts/Player Scripts/ManualHatPicker.cs
+++ Assets/Scripts/Player Scripts/ManualHatPicker.cs
@@ -4,10 +4,10 @@ using System.Collections.Generic;
 public class ManualHatPicker : MonoBehaviour
 {
     [SerializeField] private List<GameObject> hats = new List<GameObject>(); // will have 3 hats
-    [SerializeField] private int currentHatIndex = 0;
+    [SerializeField] private int numHats = 0;
     public void Start()
     {
-        ShowOnlyThisHat(0);
+        ShowHats(0);
     }
     public void OnEnable()
     {
@@ -19,17 +19,18 @@ public class ManualHatPicker : MonoBehaviour
     }
     public void CycleHat()
     {
-        currentHatIndex = (currentHatIndex + 1) % (hats.Count+1); // allows for values: 0, 1, 2, 3, then back to 0
-        ShowOnlyThisHat(currentHatIndex);
+        numHats = (numHats + 1) % (hats.Count + 1); // allows for values: 0, 1, 2, 3, then back to 0
+        ShowHats(numHats);
     }
     private void OnValidate()
     // runs whenever you change inspector values 
     {
-        ShowOnlyThisHat(currentHatIndex);
+        ShowHats(numHats);
     }
-    private void ShowOnlyThisHat(int hatIndex)
+    public void ShowHats(int numHats)
+    // Shows hats stacked on top of each other given how many hats should be displayed
     {
-        if (currentHatIndex == 0)    // hatless
+        if (numHats == 0)    // hatless
         {
             for (int i = 0; i < hats.Count; i++)
             {
@@ -38,12 +39,13 @@ public class ManualHatPicker : MonoBehaviour
         }
         else                    // hats 1,2,3
         {
-            for (int i = 1; i <= hats.Count; i++) // 
+            for (int i = 0; i <= numHats - 1; i++) // 
             {
                 // Debug.Log($"ManualHatPicker.ShowOnlyThisHat: Setting hat {i} active: {i == hatIndex + 1}");
                 // hats[] has 0,1,2 but maps to hatIndex 1,2,3. both i and hatIndex are 1-based to skip the hatless option at index 0.
-                hats[i-1].SetActive(i == hatIndex);
+                hats[i].SetActive(true);
             }
         }
     }
+
 }
